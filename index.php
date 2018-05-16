1fef08d9f4a28e4bb5fcd96ffef71b625e5a8f68
btrack <denniskipkogei@gmail.com>
2018-05-17 01:15:26 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+  <title>Cisco | Login</title>
+
+  <meta charset="utf-8">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width">
+
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700">
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,300,700">
+  <link rel="stylesheet" href="./css/font-awesome.min.css">
+  <link rel="stylesheet" href="./js/libs/css/ui-lightness/jquery-ui-1.9.2.custom.min.css">
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+    <!-- App CSS -->
+  <link rel="stylesheet" href="./css/target-admin.css">
+  <link rel="stylesheet" href="./css/custom.css">
+
+</head>
+
+<body class="account-bg">
+
+
+<hr class="account-header-divider">
+
+<div class="account-wrapper">
+
+  <div class="logo">
+    <img src="./img/cisco-logo.png">
+  </div>
+
+    <div class="account-body">
+
+
+
+      <h5 class="account-body-subtitle">SIGN IN</h5>
+
+      <form class="form account-form" method="POST" action="./index.html">
+
+        <div class="form-group">
+          <label for="login-username" class="placeholder-hidden">Username</label>
+          <input type="text" class="form-control" id="login-username" placeholder="Username" tabindex="1">
+
+        </div> <!-- /.form-group -->
+
+        <div class="form-group">
+          <label for="login-password" class="placeholder-hidden">Password</label>
+          <input type="password" class="form-control" id="login-password" placeholder="Password" tabindex="2">
+        </div> <!-- /.form-group -->
+
+        <div class="form-group clearfix">
+          <div class="pull-left">
+            <label class="checkbox-inline">
+            <input type="checkbox" class="" value="" tabindex="3">Remember me
+            </label>
+          </div>
+
+          <div class="pull-right">
+            <a href="./forgot_password.php">Forgot Password?</a>
+          </div>
+        </div> <!-- /.form-group -->
+
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary btn-block btn-lg" tabindex="4">
+            Signin &nbsp; <i class="fa fa-play-circle"></i>
+          </button>
+        </div> <!-- /.form-group -->
+
+      </form>
+
+
+    </div> <!-- /.account-body -->
+
+    <div class="account-footer">
+      <p>
+      Don't have an account? &nbsp;
+      <a href="./register.php" class="">Create an Account!</a>
+      </p>
+    </div> <!-- /.account-footer -->
+
+  </div> <!-- /.account-wrapper -->
+
+
+
+
+
+  <script src="./js/libs/jquery-1.10.1.min.js"></script>
+  <script src="./js/libs/jquery-ui-1.9.2.custom.min.js"></script>
+  <script src="./js/libs/bootstrap.min.js"></script>
+
+  <!--[if lt IE 9]>
+  <script src="./js/libs/excanvas.compiled.js"></script>
+  <![endif]-->
+  <!-- App JS -->
+  <script src="./js/target-admin.js"></script>
+
+  <!-- Plugin JS -->
+  <script src="./js/target-account.js"></script>
+
+
+
+
+
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+  <title> Cisco | Register</title>
+
+  <meta charset="utf-8">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width">
+
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700">
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,300,700">
+  <link rel="stylesheet" href="./css/font-awesome.min.css">
+  <link rel="stylesheet" href="./js/libs/css/ui-lightness/jquery-ui-1.9.2.custom.min.css">
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+    <!-- App CSS -->
+  <link rel="stylesheet" href="./css/target-admin.css">
+  <link rel="stylesheet" href="./css/custom.css">
+
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<body class="account-bg">
+
+ <!-- /.navbar -->
+
+<hr class="account-header-divider">
+
+<div class="account-wrapper">
+
+
+
+  <div class="logo">
+    <img src="./img/cisco-logo.png">
+  </div>
+
+
+    <div class="account-body">
+
+      <h3 class="account-body-title">Register</h3>
+
+
+
+      <form class="form account-form" method="POST" action="">
+
+        <div class="form-group">
+          <label for="signup-email" class="placeholder-hidden">Email Address</label>
+          <input type="text" class="form-control" id="signup-email" placeholder="Your Email" tabindex="1">
+        </div> <!-- /.form-group -->
+
+        <div class="form-group">
+          <label for="signup-fullname" class="placeholder-hidden">Your Name</label>
+          <input type="text" class="form-control" id="signup-fullname" placeholder="Your Name" tabindex="2">
+        </div> <!-- /.form-group -->
+
+        <div class="form-group">
+          <label for="signup-username" class="placeholder-hidden">Username</label>
+          <input type="text" class="form-control" id="signup-username" placeholder="Username" tabindex="3">
+        </div> <!-- /.form-group -->
+
+        <div class="form-group">
+          <label for="login-password" class="placeholder-hidden">Password</label>
+          <input type="password" class="form-control" id="login-password" placeholder="Password" tabindex="4">
+        </div> <!-- /.form-group -->
+
+
+
+        <div class="form-group">
+          <button type="submit" class="btn btn-secondary btn-block btn-lg" tabindex="6">
+          Create &nbsp; <i class="fa fa-play-circle"></i>
+          </button>
+        </div> <!-- /.form-group -->
+
+      </form>
+
+      <p>
+      Already have an account? &nbsp;
+      <a href="./index.php" class="">Login</a>
+      </p>
+    </div> <!-- /.account-body -->
+
+  </div> <!-- /.account-wrapper -->
+
+  <script src="./js/libs/jquery-1.10.1.min.js"></script>
+  <script src="./js/libs/jquery-ui-1.9.2.custom.min.js"></script>
+  <script src="./js/libs/bootstrap.min.js"></script>
+
+  <!--[if lt IE 9]>
+  <script src="./js/libs/excanvas.compiled.js"></script>
+  <![endif]-->
+  <!-- App JS -->
+  <script src="./js/target-admin.js"></script>
+
+  <!-- Plugin JS -->
+  <script src="./js/target-account.js"></script>
+
+
+
+
+
+
+</body>
+</html>
